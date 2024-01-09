913424033ea1b635373c41fce7b62ea57d8e613c
Albano João Alberto <154712167+albanojoaoalberto@users.noreply.github.com>
2024-01-09 07:47:11 +0100

    Add files via upload

---
+++ Console Aplication/Ex03/Ex03/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //CONVERSÕES DE TIPOS DE DADOS EM C#
+
+            string nome;
+            int anonasc;
+
+            //Entrada de Dados
+
+            Console.Write("Informe o Seu Nome: ");
+            nome = Console.ReadLine(); //Nota que o Dado do tipo String Não de Consersão.
+
+            Console.Write("Em que Ano Você Nasceu? :");
+            anonasc = Convert.ToInt32(Console.ReadLine());
+             
+            //Saida de Informações.
+
+            Console.WriteLine("Olá, "+nome+" Você nasceu em "+ anonasc);
+
+
+                Console.ReadKey();//Permite Fixar a tela de Console.
+        }
+    }
+}
