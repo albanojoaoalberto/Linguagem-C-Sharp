8bac7b093947c20b23fda15b10bfad7d819bc546
Albano João Alberto <154712167+albanojoaoalberto@users.noreply.github.com>
2024-01-09 07:14:09 +0100

    Add files via upload

---
+++ Ex01/Ex01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                
+            //Variaveis do tipo Constante.
+
+            String mensagem = "Olá, Mundo!";//Não Podera sofrer alteração durante a execuçao
+
+            //Variaveis do tipo Variável.
+
+            String sms; //Podera sofrer alteração durante a execuçao
+            
+        }
+    }
+}
