84a7d07452533fdce86526f2ef05ab3ae1b850ad
Albano João Alberto <154712167+albanojoaoalberto@users.noreply.github.com>
2024-01-09 06:38:59 +0100

    Add files via upload

---
+++ Console Aplication/Se livrar da maldição/Se livrar da maldição/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Se_livrar_da_maldição
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá, Mundo! ");
+        }
+    }
+}
